c607b9828543c112ae9f89d68d21488f2a96950c
Hassan Jatta <188450546+Hassan-Jatta@users.noreply.github.com>
2026-03-08 18:00:29 +0100

    security: configuration stricte du CORS pour Vercel

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  app.enableCors(); 
+  app.enableCors({
+    origin: [
+      'https://kicks-crm.vercel.app',
+      'http://localhost:3000',       
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  }); 
   
   const port = process.env.PORT || 4000;
   await app.listen(port, '0.0.0.0'); 
