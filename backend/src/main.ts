75d5b523d36e1d6b94349f30093d71303cc74a8f
Hassan Jatta <188450546+Hassan-Jatta@users.noreply.github.com>
2026-03-08 13:19:34 +0100

    fix: ajout du port dynamique et du CORS pour Render

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,8 @@ async function bootstrap() {
   
   app.enableCors(); 
   
+  const port = process.env.PORT || 4000;
   await app.listen(4000); 
+  console.log(`Backend lancé sur le port ${port}`);
 }
 bootstrap();
